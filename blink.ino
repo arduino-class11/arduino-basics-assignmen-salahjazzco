4c54cce43c6a2d54ae010d0fa77e87e938ea8849
salahjazzco <sajazz1990@gmail.com>
2025-05-25 13:41:56 +0300

    Update blink.ino

--- blink.ino
+++ blink.ino
@@ -1,11 +1,11 @@
 // Blink with errors - Students must fix it
 
 void setup() {
-  pinMode(LED_BUILTIN, INPUT);  // خطأ: المفروض OUTPUT بدل INPUT
+  pinMode(LED_BUILTIN, OUTPUT);  // خطأ: المفروض OUTPUT بدل INPUT
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);   // خطأ: المفروض HIGH لتشغيل LED
+  digitalWrite(LED_BUILTIN, HIGH);   // خطأ: المفروض HIGH لتشغيل LED
   delay(500);                       // نصف ثانية تأخير
   digitalWrite(LED_BUILTIN, HIGH);  // خطأ: المفروض LOW لإطفاء LED
   delay(500);                       // نصف ثانية تأخير
