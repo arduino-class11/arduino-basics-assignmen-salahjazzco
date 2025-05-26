19669379e2ec29a9e033144be3d990c18da1f01e
salahjazzco <sajazz1990@gmail.com>
2025-05-26 10:44:37 +0300

    Update blink.ino

--- blink.ino
+++ blink.ino
@@ -1,10 +1,8 @@
 // Blink with errors - Students must fix it
 
-
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);  // خطأ: المفروض OUTPUT بدل INPUT
 }
-
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // خطأ: المفروض HIGH لتشغيل LED
